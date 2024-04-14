9892b726c29379968d428af022d36bbedf11923a
DJeovannyZ <darwinzaldana@gmail.com>
2024-04-14 16:04:38 -0600

    startup echo

--- startup.bat
+++ startup.bat
@@ -1,3 +1,9 @@
+@echo off
+
+echo Deteniendo los contenedores y borrando imagenes creadas previamente...
+
 docker-compose down --rmi local
+
+echo Iniciando la aplicacion...
 
 docker-compose up
--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
+echo "Deteniendo los contenedores y borrando imagenes creadas previamente..."
+
 docker-compose down --rmi local
+
+echo "Iniciando la aplicacion..."
 
 docker-compose up
